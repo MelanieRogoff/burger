836b393a7ca8c8ed2fec29d70aabd453bea243de
Melanie Rogoff <mgrogoff@gmail.com>
2020-02-24 15:48:11 -0700

    Further progress on burger setup

--- db/schema.sql
+++ db/schema.sql
@@ -8,19 +8,3 @@ CREATE TABLE burgers (
   devoured BOOLEAN,
   PRIMARY KEY (id)
 );
-
-CREATE TABLE top_albums (
-  position INT NOT NULL,
-  artist VARCHAR(100) NULL,
-  album VARCHAR(100) NULL,
-  year INT NULL,
-  raw_total DECIMAL(10,4) NULL,
-  raw_usa DECIMAL(10,4) NULL,
-  raw_uk DECIMAL(10,4) NULL,
-  raw_eur DECIMAL(10,4) NULL,
-  raw_row DECIMAL(10,4) NULL,
-  PRIMARY KEY (position)
-);
-
-SELECT * FROM top5000;
-select * from top_albums;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Kosher Burger", true), ("Turkey Bacon", false), ("Classic", true);
+
+SELECT * FROM burgers;
+
